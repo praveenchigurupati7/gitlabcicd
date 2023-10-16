39bc6cb7999d1765491a089feee684604afd7876
praveen chigurupati <praveen.chigurupati@actifio.com>
2023-10-15 22:24:15 -0500

    removed unnecessary steps from startup script

--- startup_script.sh
+++ startup_script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install terraform
+sudo yum -y install terraform curl
 
 curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh" | sudo bash
 sudo yum install gitlab-runner
